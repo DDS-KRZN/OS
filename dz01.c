3ac004c3b9cd3a1e01b31534f2318aaf736431fc
Dotsenko-D <78764103+DDS-KRZN@users.noreply.github.com>
2022-09-26 17:40:33 +0300

    Add files via upload

--- dz01.c
+++ dz01.c
@@ -4,7 +4,7 @@
 #include <string.h>
 
 int rulesText(){
-	printf("За каждый ход снимается 100 кредитов.\nЕсли комбинация: 000, 111, 222, 333, 444 или 555 - вы получаете 500 кредитов.\nЕсли 5XX или 7XX (X - любое число) - вы получаете 200 кредитов.\nЕсли 55X или 75X (X - любое число) - вы получаете 300 кредитов\nЕсли 666 - с вас снимают 300 кредитов.\n777 - джекпот и конец игры\n");
+	printf("For each move, 100 credits are withdrawn.\nIf the combination is: 000, 111, 222, 333, 444 or 555 - you get 500 credits.\nIf 5XX or 7XX (X is any number) you get 200 credits. If 55X or 75X (X - any number) - you get 300 credits.\nIf 666 - 300 credits are removed from you.\n777 - jackpot and game over.\n");
 }
 
 int lotery(credits){
@@ -18,23 +18,23 @@ int lotery(credits){
 	printf("%d\n", a3);
 	if (((a1 == a2)&&(a2 == a3)) && ((a1!=6)&&(a2!=6)&&(a3!=6))) // esli combinatsia odinakova`a i net 666
 	{
-		printf("Победа! +500 кредитов\n\a");
+		printf("Your win! +500 credits\n\a");
 		credits+=500;		
 	} else if ((a1==5 && a2==5) || (a1==7 && a2==7))	// esli 55X ili 77X
 	{
-		printf("Победа! +300 кредитов\n\a");
+		printf("Your win! +300 credits\n\a");
 		credits+=300;
 	} else if (((a1==5)) || ((a1==7)))	// esli 5XX ili 7XX
 	{
-		printf("Победа! +200 кредитов\n\a");
+		printf("Your win! +200 credits\n\a");
 		credits+=200;
 	} else if (a1==6 && a2==6 && a3==6)		// esli 666
 	{
-		printf("Проигрыш! -300 кредитов\n\a");
+		printf("Your lose! -300 credits\n\a");
 		credits-=300;
 	} else if (a1==7 && a2==7 && a3==7)		// esli 777
 	{
-		printf("Джекпот!\n\a");
+		printf("Jackpot!\n\a");
 		exit(0);
 	}
 	return credits;
@@ -42,28 +42,28 @@ int lotery(credits){
 
 int main()
 {
-	setlocale(LC_ALL, "Russian");
+	//setlocale(LC_ALL, "Russian");
 	srand(time(NULL));
 
 	int credits = 5000;
 	int ch;
 	while(1){
 		if(credits < 100){
-			printf("\nНедостаточно кредитов для игры!\n\a");
+			printf("\nNot enough credits to play!\n\a");
 			exit(0);
 		}
-		printf("\n----- МЕНЮ -----\n");
-		printf("Кредитов осталось: %d\n", credits);
-		printf("\t1: Старт\n");
-		printf("\t2: Правила и награды\n");
-		printf("\t0: Выход\n");
-		printf("Ваш выбор: ");
+		printf("\n----- MENU -----\n");
+		printf("Credits left: %d\n", credits);
+		printf("\t1: Start\n");
+		printf("\t2: Rules and rewards\n");
+		printf("\t0: Exit\n");
+		printf("Your choice: ");
 		scanf("%d", &ch);
 		switch(ch){
 			case 0: exit(0);
 			case 1: credits = lotery(credits); break;
 			case 2: rulesText(); break;
-			default: printf("Введено некорректное значение!\a\n"); break;
+			default: printf("Invalid value entered!\a\n"); break;
 		}
 	}
 	return 0;
